26b5caf5fd7d9e0c6d9288613d1d487262ae300f
nicoweblab <nicoweblab@gmail.com>
2025-06-18 14:49:31 -0300

    corales

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("ey quieres un coral")
+confirm("era bait")
+prompt("te gustan los nuggets")
+const botton = document.getElementById('el boton');
+
+
+
+botton.addEventListener('click'), () = {
+    window.location.href = "https://fortnite.gg/"
+
+
+
+
+
+
+
+
